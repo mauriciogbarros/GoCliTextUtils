2458e7787fe44f3b145e4a12f38fc64ffdf7c361
Mauricio <mauricio.gbarros@outlook.com>
2026-06-15 01:36:29 +0200

    Implement hashing

--- gopwdutil/go.mod
+++ gopwdutil/go.mod
@@ -2,6 +2,8 @@ module gopwdutil
 
 go 1.26.3
 
+require golang.org/x/crypto v0.53.0
+
 require (
 	github.com/ebitengine/purego v0.10.1 // indirect
 	golang.design/x/clipboard v0.8.0 // indirect
@@ -9,5 +11,5 @@ require (
 	golang.org/x/exp/shiny v0.0.0-20250606033433-dcc06ee1d476 // indirect
 	golang.org/x/image v0.28.0 // indirect
 	golang.org/x/mobile v0.0.0-20250606033058-a2a15c67f36f // indirect
-	golang.org/x/sys v0.33.0 // indirect
+	golang.org/x/sys v0.46.0 // indirect
 )
--- gopwdutil/transform/Hash.go
+++ gopwdutil/transform/Hash.go
@@ -2,10 +2,30 @@ package transform
 
 import (
 	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 func Hash(ppwd *[]byte) {
 	if ppwd == nil { return }
+	hash, err := bcrypt.GenerateFromPassword(*ppwd, bcrypt.DefaultCost)
+
+	if err != nil {
+		fmt.Println("Error: failed to hash.")
+		return
+	}
 	
-	fmt.Println("Hash password: under construction")
+	err = bcrypt.CompareHashAndPassword(hash, *ppwd)
+	if err != nil {
+		fmt.Println("Error: hash and password don't match")
+		return
+	}
+
+	fmt.Println("Password hashed")
+	for i := range *ppwd { (*ppwd)[i] = 0 }
+	for i := range hash { 
+		*ppwd = append(*ppwd, hash[i])
+		hash[i] = 0
+	}
+	fmt.Println("Hash erased")
 }
